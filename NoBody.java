e1605c260a2b9a2fab2ae90e0e59beafa8894cb7
21bq1a4227 <113082928+21bq1a4227@users.noreply.github.com>
2022-10-11 04:43:50 +0530

    Create NoBody.java

---
+++ NoBody.java
@@ -0,0 +1,14 @@
+public class NoBody {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i, j;
+		 i = 100;
+		 j = 200;
+		 // find midpoint between i and j
+		 while(++i < --j); // no body in this loop
+		 System.out.println("Midpoint is " + i);
+
+	}
+
+}
